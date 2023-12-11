85f4db96c89c7d833389abd6610b5ba63f79a836
grayson.han <1036449976@qq.com>
2023-12-12 01:33:13 +0800

    Del: 删除多余文件,添加选择排序

--- SortingAlgorithm/InsertionSort/InsertionSort.java
+++
@@ -1,7 +0,0 @@
-
-//1.选择排序
-//时间复杂度 O(n*2)， 额外空间复杂度O(1)
-
-public class InsertionSort {
-    
-}
---
+++ SortingAlgorithm/SelectionSort/SelectionSort.java
@@ -0,0 +1,45 @@
+
+//1.选择排序
+//时间复杂度 O(n*2)， 额外空间复杂度O(1)
+
+public class SelectionSort {
+    public static void SelectionSortSelf(int[] arr) {
+        if(arr == null || arr.length<2){
+            return;
+        }
+        //遍历0~N-1，每次把最小值放在i位置
+        for(int i = 0; i < arr.length-1; i++){
+            int minIndex = i;
+            //i~N-1寻找最小值
+            for(int j = i + 1; j < arr.length ; j++){             
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            swap1(arr, i, minIndex);
+        }
+
+    }
+
+    public static void swap1(int[] arr, int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+
+    //异或交换两个不同地址的数
+    public static void swap2(int[] arr, int a, int b) {
+        arr[a] = arr[a]^arr[b];
+        arr[b] = arr[a]^arr[b];
+        arr[a] = arr[a]^arr[b];
+    }
+
+    //计数器For test 
+    public static void main(String[] args) {
+        int[] arr1 = {3,2,5,6,1,9,10,7,5,6,2,3,15};
+        SelectionSortSelf(arr1);
+        for(int i = 0; i < arr1.length - 1; i++)
+        {
+            System.out.println(arr1[i]);
+        }
+        
+    }
+}
