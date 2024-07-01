6e7a1910ed1cbc8bc96af2a529d8e8a5bc285f2b
pikapikaqiua <1036449976@qq.com>
2024-07-02 03:25:25 +0800

    滑动窗口相关

---
+++ SlidingWindow/SlidingWindow.java
@@ -0,0 +1,17 @@
+// 模板
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlidingWindow {
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "ababab";
+        String p = "1ab";
+        findAnagrams(s, p);
+    }
+}
